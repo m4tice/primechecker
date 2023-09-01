e5a4987e5a741bec63230352ba570078a91ed672
m4tice <44093611+SledgyNguyen@users.noreply.github.com>
2023-09-01 14:08:14 +0530

    Create test input and Update settings

---
+++ src/utils/settings.py
@@ -0,0 +1,4 @@
+"""settings"""
+
+KEYWORD_BM = "BM"
+BM_NUMBER_LEN = 15
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,10 @@
 """Test ultilities"""
 
+import pandas as pd
+
 from src.utils.utils import get_none
+from src.utils.utils import get_account_numbers_by_id
+from src.utils.utils import get_account_numbers_by_sample
 
 
 def test_get_none():
@@ -9,3 +13,19 @@ def test_get_none():
     Expected return of get_none() is 'None'
     """
     assert get_none() is None
+
+
+def test_get_account_numbers_by_id(data_frame: pd.DataFrame, rq1id: str):
+    """
+    Expectation: function is able to filter BM numbers
+    from input of data frame using RQ1ID
+    """
+    pass
+
+
+def test_get_account_numbers_by_sample(input_string: str):
+    """
+    Expectation: function is able to filter BM numbers
+    from input of data frame using sample input string
+    """
+    pass
