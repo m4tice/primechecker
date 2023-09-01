5bc0d6d1130b412b8ac5ed16e2e5277e2a57eebd
m4tice <44093611+SledgyNguyen@users.noreply.github.com>
2023-09-01 14:58:49 +0530

    Update enable pytest

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,44 +1,44 @@
 """Ultilities"""
 
-# import pandas as pd
+import pandas as pd
 
-# from utils.settings import KEYWORD_BM, BM_NUMBER_LEN
-
-
-# def get_none():
-#     """
-#     This function return None
-#     Its purpose is for pytest GitHub Actions demo
-#     """
-#     return None
+from utils.settings import KEYWORD_BM, BM_NUMBER_LEN
 
 
-# def get_account_numbers_by_id(data_frame: pd.DataFrame, rq1id: str):
-#     """
-#     get account numbers from dataframe with id
-#     """
-#     sample = data_frame.loc[rq1id]['AccountNumbers']
-
-#     if KEYWORD_BM in sample:
-#         sample_split = sample.split("\n")
-#         account_numbers = [item[item.find("BM"):item.find("BM") + BM_NUMBER_LEN] \
-#                         for item in sample_split if item != '']
-
-#         return account_numbers
-
-#     return "-"
+def get_none():
+    """
+    This function return None
+    Its purpose is for pytest GitHub Actions demo
+    """
+    return None
 
 
-# def get_account_numbers_by_sample(input_string: str):
-#     """
-#     get account numbers from input string
-#     """
+def get_account_numbers_by_id(data_frame: pd.DataFrame, rq1id: str):
+    """
+    get account numbers from dataframe with id
+    """
+    sample = data_frame.loc[rq1id]['AccountNumbers']
 
-#     if KEYWORD_BM in input_string:
-#         sample_split = input_string.split("\n")
-#         account_numbers = [item[item.find("BM"):item.find("BM") + BM_NUMBER_LEN] \
-#                         for item in sample_split if item != '']
+    if KEYWORD_BM in sample:
+        sample_split = sample.split("\n")
+        account_numbers = [item[item.find("BM"):item.find("BM") + BM_NUMBER_LEN] \
+                        for item in sample_split if item != '']
 
-#         return account_numbers
+        return account_numbers
 
-#     return "-"
+    return "-"
+
+
+def get_account_numbers_by_sample(input_string: str):
+    """
+    get account numbers from input string
+    """
+
+    if KEYWORD_BM in input_string:
+        sample_split = input_string.split("\n")
+        account_numbers = [item[item.find("BM"):item.find("BM") + BM_NUMBER_LEN] \
+                        for item in sample_split if item != '']
+
+        return account_numbers
+
+    return "-"
