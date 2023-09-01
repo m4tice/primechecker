2720ac1e818aa1809c74dcaebca966aea7252964
m4tice <44093611+SledgyNguyen@users.noreply.github.com>
2023-09-01 16:54:20 +0530

    Update test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,12 +7,14 @@ from src.utils.utils import get_none
 from src.utils.utils import get_account_numbers_by_id
 # from src.utils.utils import get_account_numbers_by_sample
 
+
 from tests.test_settings import TEST_FILE_PATH
 from tests.test_settings import TEST_BM_NUMBER_EXTRACT_RQ1ID_01, TEST_BM_NUMBER_EXTRACT_RQ1ID_02, TEST_BM_NUMBER_EXTRACT_RQ1ID_03
 from tests.test_settings import TEST_BM_NUMBER_EXTRACT_RESULT_01, TEST_BM_NUMBER_EXTRACT_RESULT_02, TEST_BM_NUMBER_EXTRACT_RESULT_03
 
 
 test_data = pd.read_excel(TEST_FILE_PATH)
+test_data = test_data.set_index("id")
 
 
 def test_get_none():
@@ -38,6 +40,3 @@ def test_get_account_numbers_by_sample():
     Expectation: function is able to filter BM numbers
     from input of data frame using sample input string
     """
-
-
-test_get_account_numbers_by_id()
