86ad25470cb044fc86381351623dca14ae28eb06
m4tice <44093611+SledgyNguyen@users.noreply.github.com>
2023-09-01 16:57:18 +0530

    Update test_file_path

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,6 +1,6 @@
 """settings for pytest"""
 
-TEST_FILE_PATH = "./test_data/test_QueryResult.xlsx"
+TEST_FILE_PATH = "tests/test_data/test_QueryResult.xlsx"
 
 TEST_BM_NUMBER_EXTRACT_RQ1ID_01 = "RQONE02218228"
 TEST_BM_NUMBER_EXTRACT_RQ1ID_02 = "RQONE03133330"
